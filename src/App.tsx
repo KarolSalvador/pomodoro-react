82a9a975a939ccffb71c54878ded40f696197599
KarolSalvador <karolsalvador91@gmail.com>
2025-10-23 09:08:42 -0300

    feat:cria container CountDown, crian component DefaultInput

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Container } from './components/Container';
+import { CountDown } from './components/CountDown';
+import { DefaultInput } from './components/DefaultInput';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 
@@ -11,8 +13,34 @@ export function App() {
       <Container>
         <Logo />
       </Container>
+
       <Container>
         <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
+      </Container>
+
+      <Container>
+        <form action='' className='form'>
+          <div className='formRow'>
+            <DefaultInput type='text' id='input' labelText='Task' />
+          </div>
+
+          <div className='formRow'>
+            <p>Nesse ciclo foque por 25 min.</p>
+          </div>
+
+          <div className='formRow'>
+            <p>Ciclos</p>
+            <p>0 0 0 0</p>
+          </div>
+
+          <div className='formRow'>
+            <button>Play</button>
+          </div>
+        </form>
       </Container>
     </>
   );
