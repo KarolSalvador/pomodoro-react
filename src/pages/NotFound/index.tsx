c03f559ea3516946fe1f9bb7991a9746c88c5360
KarolSalvador <karolsalvador91@gmail.com>
2025-10-24 16:39:37 -0300

    feat: Cria escopo de página not found

---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,17 @@
+import { Container } from '../../components/Container';
+import { MainTemplate } from '../../templates/MainTemplate';
+
+export function NotFound() {
+  return (
+    <MainTemplate>
+      <Container>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel quos
+          voluptate reprehenderit eos nesciunt unde veniam, ea adipisci possimus
+          harum nam explicabo reiciendis alias nemo provident ratione temporibus
+          culpa sequi.
+        </p>
+      </Container>
+    </MainTemplate>
+  );
+}
