80341a142ba3ef6ea86166b1971e2ae103d6f4eb
KarolSalvador <karolsalvador91@gmail.com>
2025-10-24 14:57:12 -0300

    refact: Adiciona data-theme a index.html, adiciona aria label e title aos icones do Menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -5,16 +5,32 @@ export function Menu() {
   return (
     <>
       <nav className={styles.menu}>
-        <a className={styles.menuLink}>
+        <a
+          className={styles.menuLink}
+          aria-label='Ir para a Home'
+          title='Ir para a Home'
+        >
           <HouseIcon />
         </a>
-        <a className={styles.menuLink}>
+        <a
+          className={styles.menuLink}
+          aria-label='Ver Histórico'
+          title='Ver Histórico'
+        >
           <HistoryIcon />
         </a>
-        <a className={styles.menuLink}>
+        <a
+          className={styles.menuLink}
+          aria-label='Ir para Configurações'
+          title='Ir para Configurações'
+        >
           <SettingsIcon />
         </a>
-        <a className={styles.menuLink}>
+        <a
+          className={styles.menuLink}
+          aria-label='Mudar Tema'
+          title='Mudar Tema'
+        >
           <SunIcon />
         </a>
       </nav>
