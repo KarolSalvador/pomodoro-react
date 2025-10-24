672249384ac86cb3c03269db8c867c01b0f95985
KarolSalvador <karolsalvador91@gmail.com>
2025-10-24 08:29:21 -0300

    feat: Atualiza App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { PlayCircleIcon } from 'lucide-react';
 import { Container } from './components/Container';
 import { CountDown } from './components/CountDown';
+import { Cycles } from './components/Cycles';
+import { DefaultButton } from './components/DefaultButton';
 import { DefaultInput } from './components/DefaultInput';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 
 import './styles/global.css';
 import './styles/theme.css';
+import { Footer } from './components/Footer';
 
 export function App() {
   return (
@@ -25,7 +29,12 @@ export function App() {
       <Container>
         <form action='' className='form'>
           <div className='formRow'>
-            <DefaultInput type='text' id='input' labelText='Task' />
+            <DefaultInput
+              type='text'
+              id='input'
+              labelText='Task:'
+              placeholder='Ex: Estudar para a prova'
+            />
           </div>
 
           <div className='formRow'>
@@ -33,14 +42,17 @@ export function App() {
           </div>
 
           <div className='formRow'>
-            <p>Ciclos</p>
-            <p>0 0 0 0</p>
+            <Cycles />
           </div>
 
           <div className='formRow'>
-            <button>Play</button>
+            <DefaultButton icon={<PlayCircleIcon />} />
           </div>
         </form>
+      </Container>
+
+      <Container>
+        <Footer />
       </Container>
     </>
   );
